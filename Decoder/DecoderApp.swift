f91d5617f1458e13976d694c3cc28a78c7f2d406
Tom Hartnett <tomhartnett@gmail.com>
2021-12-26 18:34:11 -0600

    Initial Commit

---
+++ Decoder/DecoderApp.swift
@@ -0,0 +1,17 @@
+//
+//  DecoderApp.swift
+//  Decoder
+//
+//  Created by Tom Hartnett on 12/26/21.
+//
+
+import SwiftUI
+
+@main
+struct DecoderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
